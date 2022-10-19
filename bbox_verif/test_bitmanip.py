00ccd117e7a9e4f986cc902dcb943d6b2440094f
ssanthoshpavan <santhosh@mindgrovetech.in>
2022-10-19 20:09:14 +0530

    updated template provides log info

--- bbox_verif/test_bitmanip.py
+++ bbox_verif/test_bitmanip.py
@@ -6,10 +6,11 @@ from cocotb.decorators import coroutine
 from cocotb.triggers import Timer, RisingEdge, FallingEdge
 from cocotb.binary import BinaryValue
 from cocotb.clock import Clock
+from cocotb.regression import TestFactory
 
 from bitmanip_ref_model import bitmanip_rm
 
-#// Ports:
+#// DUT Ports:
 #// Name                         I/O  size props
 #// bbox_out                       O    65
 #// CLK                            I     1 unused
@@ -17,42 +18,37 @@ from bitmanip_ref_model import bitmanip_rm
 #// instr                          I    32
 #// rs1                            I    64
 #// rs2                            I    64
-#//
-#// Combinational paths from inputs to outputs:
 #//   (instr, rs1, rs2) -> bbox_out
 
-'''
-#Reference model
-def bitmanip_rm(instr, rs1, rs2):
-    if instr == 1:
-        res = rs1 & ~rs2
-        result = '{:064b}'.format(res)
-        return '1'+result
-'''
+
 #generates clock and reset
 async def initial_setup(dut):
 	cocotb.start_soon(Clock(dut.CLK, 1, units='ns').start())
         
 	dut.RST_N.value = 0
 	await RisingEdge(dut.CLK)
-	await RisingEdge(dut.CLK)
 	dut.RST_N.value = 1
 
 
 #drives input data to dut
-async def input_driver(dut, instr, rs1, rs2):
+async def input_driver(dut, instr, rs1, rs2, single_opd):
     await RisingEdge(dut.CLK)
     dut.instr.value = instr
     dut.rs1.value = rs1
-    dut.rs2.value = rs2
-    await RisingEdge(dut.CLK)
     dut._log.info("---------------- DUT Input Info -----------------------")
-    dut._log.info("instr = %s  rs1 = %s rs2 = %s",hex(dut.instr.value), hex(dut.rs1.value), hex(dut.rs2.value))
+    if single_opd == 1:
+        await RisingEdge(dut.CLK)
+        dut._log.info("instr = %s  rs1 = %s ",hex(dut.instr.value), hex(dut.rs1.value))
+
+    else :
+        dut.rs2.value = rs2
+        await RisingEdge(dut.CLK)
+        dut._log.info("instr = %s  rs1 = %s rs2 = %s",hex(dut.instr.value), hex(dut.rs1.value), hex(dut.rs2.value))
     dut._log.info("-------------------------------------------------------")
+
 #monitors dut output
 async def output_monitor(dut):
     while True:
-        #print(dut.instr.value, dut.rs1.value, dut.rs2.value)
         await RisingEdge(dut.CLK)
         if(dut.bbox_out.value[0]): break
 
@@ -67,31 +63,41 @@ async def scoreboard(dut, dut_result, rm_result):
     assert rm_result == str(dut_result),"Failed"
     dut._log.info("-------------------------------------------------------")
 
-
-#testbench 
-async def TB(dut, instr, rs1, rs2):
-    rm_result = bitmanip_rm(instr, rs1, rs2)
-    
-    await input_driver(dut, instr, rs1, rs2)
-    dut_result = await output_monitor(dut)
-    
-    await scoreboard(dut, dut_result, rm_result)	
-
-#test vectors
-@cocotb.test()
-async def test_addn(dut):
+#Testbench
+async def TB(dut, XLEN, instr, instr_name, single_opd, num_of_tests):
     await initial_setup(dut)
-    for i in range (10):
-        instr = 1
-        rs1 = random.randint(0,2**64) 
-        rs2 = random.randint(0,2**64)
-        await TB(dut, instr, rs1, rs2)
-@cocotb.test()
-async def test_addn_2(dut):
+    dut._log.info("*******************************************************")
+    dut._log.info("------------- Test %r of RV%d starts --------------" %(instr_name,XLEN))
+    dut._log.info("*******************************************************")
+    for i in range (num_of_tests):
+        rs1 = random.randint(0,(2**XLEN)-1) 
+        rs2 = random.randint(0,(2**XLEN)-1)
+        rm_result = bitmanip_rm(instr, rs1, rs2, XLEN)
+    
+        await input_driver(dut, instr, rs1, rs2, single_opd)
+        dut_result = await output_monitor(dut)
+    
+        await scoreboard(dut, dut_result, rm_result)	
+    dut._log.info("*******************************************************")
+    dut._log.info("------------- Test %r of RV%d ends ----------------" %(instr_name,XLEN))
+    dut._log.info("*******************************************************")
 
-    await initial_setup(dut)
-    instr = 1
-    rs1 = 0x1111
-    rs2 = 0x1011
-    await TB(dut, instr, rs1, rs2)
+
+# generates sets of tests based on the different permutations of the possible arguments to the test function
+tf = TestFactory(TB)
+
+base = 'RV64'
+
+if base == 'RV32':
+    tf.add_option('XLEN', [32])
+    tf.add_option(('instr','instr_name','single_opd'), [(1, 'addn', 0)])
+    ##To run multiple instr - tf.add_option(((('instr','instr_name','single_opd'), [(1, 'addn', 0),(2,'clz',1),(...)])
+
+elif base == 'RV64':
+    tf.add_option('XLEN', [64])
+    tf.add_option(('instr','instr_name','single_opd'), [(1, 'addn', 0)])
+    ##To run multiple instr - tf.add_option(((('instr','instr_name','single_opd'), [(1, 'addn', 0),(2,'clz',1),(...)])
+
+tf.add_option('num_of_tests',[10])
+tf.generate_tests()
 
